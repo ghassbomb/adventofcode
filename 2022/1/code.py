dd99177641df77282f10171ed8f7c29133541938
fr06t <ghass.shahzad@gmail.com>
2022-12-30 00:26:28 +0500

    first done; readme added

--- 2022/1/code.py
+++ 2022/1/code.py
@@ -3,12 +3,26 @@
 # Date = December 2022
 
 with open((__file__.rstrip("code.py")+"input.txt"), 'r') as input_file:
-    input = input_file.read()
+    input = input_file.readlines()
+    most_cal = 0
+    total_cal = 0
+    secondmost_cal = 0 
+    thirdmost_cal = 0
+    ttotal_cal = 0
+    for line in input:
+        if line.strip():
+           total_cal += int(line) 
+        else:
+            if total_cal > most_cal:
+                secondmost_cal = most_cal
+                most_cal = total_cal
+            elif total_cal > secondmost_cal:
+                thirdmost_cal = secondmost_cal
+                secondmost_cal = total_cal
+            elif total_cal > thirdmost_cal:
+                thirdmost_cal = total_cal
+            total_cal = 0
+    ttotal_cal = most_cal + secondmost_cal + thirdmost_cal
 
-
-
-print("Part One : "+ str(None))
-
-
-
-print("Part Two : "+ str(None))
+print("Part One : "+ str(most_cal))
+print("Part Two : "+ str(ttotal_cal))
